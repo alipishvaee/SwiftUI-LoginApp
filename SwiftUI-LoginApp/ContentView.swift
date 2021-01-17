1924d3ebdf8801316f886f13441b565f4607f23e
Ali Pishvaee <alborzihoseinali@gmail.com>
2021-01-17 13:24:09 +0330

    Initial Commit

---
+++ SwiftUI-LoginApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-LoginApp
+//
+//  Created by Ali Pishvaee on 1/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-LoginApp/SwiftUI_LoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LoginAppApp.swift
+//  SwiftUI-LoginApp
+//
+//  Created by Ali Pishvaee on 1/17/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
